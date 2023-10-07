0e7d6a004267258257987ba48d3df6f98e4c8f27
mperehinets <mar.perehinets@gmail.com>
2023-10-07 22:22:37 +0300

    push

--- dz/src/Main.java
+++ dz/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Print printer = new Print();
+        printer.print();
     }
 }
---
+++ dz/src/Print.java
@@ -0,0 +1,7 @@
+public class Print {
+    public int a = 10;
+
+    public void print(){
+        System.out.printf("---<%d>---", a);
+    }
+}
